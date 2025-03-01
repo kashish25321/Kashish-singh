9893bc8e584d425878b9d8b32d6509e818139bdb
kashish25321 <kashishsingh.ml24@sbjit.edu.in>
2025-03-01 17:06:23 +0530

    Add files via upload

---
+++ 2nd practical.h
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <title>
+            practical 2
+        </title>
+    </head>
+    <body>
+        <h4>Links & Hyperlinks </h4>
+        <--Ordered list-->
+            <h2>Ordered List</h2>
+            <ol>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Bootstrap</li>
+            </ol>
+            <--Unordered List-->
+                <h2>Unordered List</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Bootstrap</li>
+            </ul>
+            <--Descrption list-->
+                <h2>Description of Trem1</h2><>
+                <d1>
+                    <dt>Term1</dt>
+                    <dd>Description of Trem1</dd>
+                    <dt>Term2</dt>
+                    <dd>Description of Trem2</dd>
+                </d1>
+                
+                <--Internal Link-->
+                    <a href="#top">Go to Top</a>
+                <--External Link-->
+                    <a href="https://www.google.com" target="blank">Visit Google</a>
+                <--Email Link-->
+                    <a href="mailto:example@example.com">If any query mail us</a>
+        </body>
+</html>
